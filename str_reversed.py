eaffb46b0fee3ecefdeafa318aa58cbadbb91143
Pol <pixelliot@gmail.com>
2016-01-26 15:04:57 +0300

    Add new reversed function

--- str_reversed.py
+++ str_reversed.py
@@ -1,5 +1,16 @@
 def reverse(text):
+	#Returns the reverse of a string using slicing
 	return text[::-1]
+
+def swap(list_, i, j):
+	list_[i], list_[j] = list_[j], list_[i]
+
+def reversex(text):
+	text = list(text)
+	for i in range(len(text)//2):
+		swap(text, text[i], text[len(text)-(i+1)])
+		
+	return "".join(text)
 
 #Examples
 reverse("Pol")
