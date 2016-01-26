a725b0c33aa71b3f621c7352c9694bc57f3fecd2
Paul <pixelliot@gmail.com>
2016-01-26 20:45:49 +0300

    Update bronchez.py

--- bronchez.py
+++ bronchez.py
@@ -3,6 +3,6 @@
 while True:
     try:
         x = raw_input("What is your name?")
-        break
+        break   #If user enters a string then the loop breaks (exits)
     except ValueError:
-        print "Oops! please enter a name."
+        print "Oops! please enter a name."  #From this point the loop repeats itself
